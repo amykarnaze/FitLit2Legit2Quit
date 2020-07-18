bf0fac9935f304da33b7b05a22e13d3716494985
Ben McClung <aurumvalian@gmail.com>
2020-07-18 14:15:29 -0600

    Add line breaks between methods in Activity

--- src/Activity.js
+++ src/Activity.js
@@ -9,18 +9,21 @@ class Activity {
     this.reachedStepGoal = null;
     this.doActivity(userRepository);
   }
+
   doActivity(userRepo) {
     var activity = this;
     userRepo.users.find(function(user) {
       return user.id === activity.userId;
     }).updateActivities(this);
   }
+
   calculateMiles(userRepository) {
     let walkingUser = userRepository.users.find(user => {
       return user.id === this.userId;
     });
     return Math.round(this.steps * walkingUser.strideLength / 5280).toFixed(1);
   }
+
   compareStepGoal(userRepository) {
     let userStepGoal = userRepository.users.find(user => {
       return user.id === this.userId;
