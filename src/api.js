924412cad7b7ba414cef62efe3571441e1769372
Amy Karnaze <akarnaze@gmail.com>
2020-07-20 14:44:04 -0600

    Add sleep api

--- src/api.js
+++ src/api.js
@@ -2,3 +2,7 @@ const userData = fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/users/u
   .then(response => response.json())
   .catch(error => console.log(error));
 
+const sleepData = fetch('https: //fe-apps.herokuapp.com/api/v1/fitlit/1908/sleep/sleepData')
+  .then(response => response.json())
+  .catch(error => console.log(error));
+
