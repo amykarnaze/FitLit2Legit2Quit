2955be9789dd3f4a42c06283f07134e9ca7845b7
Ben McClung <aurumvalian@gmail.com>
2020-07-21 13:16:55 -0600

    Add data chain into fetch function

--- src/api.js
+++ src/api.js
@@ -1,17 +1,21 @@
 const userData = fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/users/userData')
   .then(response => response.json())
+  .then(data => data.userData)
   .catch(error => console.log(error));
 
 const sleepData = fetch('https: //fe-apps.herokuapp.com/api/v1/fitlit/1908/sleep/sleepData')
   .then(response => response.json())
+  .then(data => data.sleepData)
   .catch(error => console.log(error));
 
 const activityData = fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/activity/activityData')
   .then(response => response.json())
+  .then(data => data.activityData)
   .catch(error => console.log(error));
 
 fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/hydration/hydrationData')
   .then(response => response.json())
+  .then(data => data.hydrationData)
   // .then(data => put data into hydration function so you can manipulate it hydration(data))
   .catch(error => console.log(error));
 
